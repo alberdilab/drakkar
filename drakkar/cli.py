adb16027e78a354fd041578753865997f4607a91
anttonalberdi <anttonalberdi@gmail.com>
2025-02-13 12:44:33 +0100

    d

--- drakkar/cli.py
+++ drakkar/cli.py
@@ -40,7 +40,7 @@ def run_snakemake_preprocessing(workflow, input_dir, output_dir):
         "/bin/bash", "-c",  # Ensures the module system works properly
         f"module load {config_vars['SNAKEMAKE_MODULE']} && "
         "snakemake "
-        f"-s {PACKAGE_DIR / 'workflow' / 'Snakefile')} "
+        f"-s {PACKAGE_DIR / 'workflow' / 'Snakefile'} "
         f"--configfile {CONFIG_PATH}"
         f"--config workflow={workflow} reads_dir={input_dir} output_dir={output_dir}"
     ]
